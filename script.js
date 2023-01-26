20c44dfd80d168774eca0bce1e7e3941ae96d8f3
Ernesto <ernesto.gt90@gmail.com>
2023-01-26 16:40:59 -0500

    Message generator

---
+++ script.js
@@ -0,0 +1,19 @@
+const generarNum = (num) => {
+    return Math.floor(Math.random() * num);
+}
+const messages = {
+    energizing: ['You got this!', 'Remember to take breaks', 'You are doing great!'],
+    luck: ['You are lucky today', 'You are lucky this week', 'You are lucky this month'],
+    love: ['You are loved', 'You are loved by your family', 'You are loved by your friends']
+};
+
+const messageGenerator = (obj) => {
+    let message = '';
+    for (let key in obj) {
+        let index = generarNum(obj[key].length);
+        message += `${obj[key][index]}` + ".\t";
+    }
+    return message;
+}
+
+console.log(messageGenerator(messages));
